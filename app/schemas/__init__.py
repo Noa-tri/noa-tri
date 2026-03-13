d949d25206962638a759723a36eee869ac4ac92b
Noa-tri <proferodrigomanuel@gmail.com>
2026-03-13 19:10:42 -0300

    Create __init__.py

---
+++ app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from app.schemas.athlete import AthleteCreate, AthleteResponse
+from app.schemas.training_session import TrainingSessionCreate, TrainingSessionResponse
+from app.schemas.biomarker import BiomarkerCreate, BiomarkerResponse
+from app.schemas.risk import RiskResponse
