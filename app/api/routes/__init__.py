ffb869eab6383aec6cc81cfcfaae7b5c8a7a31bd
Noa-tri <proferodrigomanuel@gmail.com>
2026-03-13 19:18:55 -0300

    Create __init__.py

---
+++ app/api/routes/__init__.py
@@ -0,0 +1,5 @@
+from app.api.routes.athletes import router as athletes_router
+from app.api.routes.sessions import router as sessions_router
+from app.api.routes.biomarkers import router as biomarkers_router
+from app.api.routes.analytics import router as analytics_router
+from app.api.routes.risk import router as risk_router
